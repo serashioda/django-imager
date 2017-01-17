30c0aadb8994ade9d80c2db0354afc23fe8fdc06
Ben Petty <benjamin.s.petty@gmail.com>
2017-01-17 00:53:38 -0800

    start on tests

--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -1,3 +1,52 @@
+"""Imager Profile Tests."""
+
 from django.test import TestCase
+from django.contrib.auth.models import User
+from imager_profile.models import ImagerProfile
+import factory
 
 # Create your tests here.
+
+
+class ProfileTestCase(TestCase):
+    """Profile model test runner."""
+
+    class UserFactory(factory.django.DjangoModelFactory):
+        """Factory for building new user objects."""
+
+        class Meta:
+            """Setup factory model."""
+
+            model = User
+
+        username = factory.Sequence(lambda n: "Pho To Combo {}".format(n))
+
+    def setUp(self):
+        """Setup for the test."""
+        self.users = [self.UserFactory.create() for i in range(20)]
+
+    def test_model_string(self):
+        """String.
+
+        Ensure that each of your models has a string representation that
+        appropriately displays it when using the Django shell.
+        """
+        pass
+
+    def test_user_gets_imgr_profile(self):
+        """Test User gets Imager Profile.
+
+        Ensure that every standard Django user object created automatically
+        gets an ImagerProfile.
+        """
+        user = self.users[0]
+        self.assertTrue(hasattr(user, 'profile'))
+        self.assertIsInstance(user.profile, ImagerProfile)
+
+    def test_del_user_on_db_and_imgr(self):
+        """Test delete user on DB & Imgr.
+
+        Ensure that if a user is deleted from the database, the ImagerProfile
+        associated with that user is also deleted.
+        """
+        pass
