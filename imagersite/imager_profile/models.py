4bdbea6bc46f9054981d74605e505ad555ad9454
Your Name <you@example.com>
2017-01-16 18:20:30 -0800

    models

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+    """."""
+
+class Meta(AbstractUser.Meta):
+    swappable = 'AUTH_USER_MODEL'
+
+class AbstractUser(AbstractBaseUser, PermissionsMixin):
+    """."""
+    username = models.CharField(
+        _('username'),
+        max_length=30,
+        unique=True,
+        help_text=_('Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.')
+        )
