419ba56b90858cf11c49216595e2d01d0f26b79a
Ben Petty <benjamin.s.petty@gmail.com>
2017-01-17 18:57:03 -0800

    incl home route

--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -1,4 +1,4 @@
-"""imagersite URL Configuration
+"""imagersite URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from imagersite import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home_view, name='home')
 ]
