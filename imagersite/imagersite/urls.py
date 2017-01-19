065c85e67309615be56f3f11d8420be1cdf2c509
Your Name <you@example.com>
2017-01-18 23:41:32 -0800

    urls.py updated.

--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -13,9 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+from imagersite import views
+
 
 urlpatterns = [
+    url(r'^$', views.home_view, name='home'),
+    url(r'^accounts/', include('registration.backends.hmac.urls')),
+    url(r'^login/', auth_views.login, name='login'),
+    url(r'^logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     url(r'^admin/', admin.site.urls),
+    url(r'^profile/', include('imager_profile.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
