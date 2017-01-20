7276f4aaa71e9d8eafc031d275fb11a863a9f941
Ben Petty <benjamin.s.petty@gmail.com>
2017-01-19 19:54:20 -0800

    EMAIL_BACKEND in development prints to console

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -144,8 +144,8 @@ EMAIL_HOST_USER = 'imager@gmail.com'
 EMAIL_HOST_PASSWORD = 'notthatpassword'
 DEFAULT_FROM_EMAIL = 'imager@gmail.com'
 
-# if DEBUG:
-#     EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
 MEDIA_URL = "/media/"
