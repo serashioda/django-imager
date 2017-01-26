dc440aadf89ecf06570087674d5131827155f2a4
Your Name <you@example.com>
2017-01-25 18:58:57 -0800

    basic model structure/classes for user image management

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -1,5 +1,33 @@
+"""Model for user image management."""
+
 from __future__ import unicode_literals
 
 from django.db import models
+# from django.contrib.auth.models import User
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+PUBLISHED_OPTIONS = (
+    ('public', 'public'),
+    ('shared', 'shared'),
+    ('private', 'private'),
+)
+
+
+def image_path(instance, file_name):
+    """Upload file for user in media root."""
+    return 'user_{0}/{1}'.format(instance.user.id, file_name)
+
+
+@python_2_unicode_compatible
+class Photo(models.Model):
+    """Photo Model."""
+
+
+@python_2_unicode_compatible
+class Album(models.Model):
+    """Album Model."""
+
+
+def __str__(self):
+    """String Representation of User media."""
