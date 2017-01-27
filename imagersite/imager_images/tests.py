a0326007f0904bd276d6c915ecfd43d7df0c0da4
Ben Petty <benjamin.s.petty@gmail.com>
2017-01-26 23:05:15 -0800

    imager_images tests step 1

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -1,21 +1,20 @@
 """Tests for imager_images app."""
 
-from django.test import TestCase
-from django.db import models
-from django.test import SimpleTestCase
-from django.test.utils import isolate_apps
+# from django.test import TestCase
+# from django.db import models
+# from django.test import SimpleTestCase
+# from django.test.utils import isolate_apps
 
 
 # Create your tests here.
 #  -- example test ---
-@isolate_apps('app_label', attr_name='apps')
-class TestModelDefinition(SimpleTestCase):
-    """."""
+# @isolate_apps('app_label', attr_name='apps')
+# class TestModelDefinition(SimpleTestCase):
+#     """."""
 
-    def test_model_definition(self):
-        """."""
-        class TestModel(models.Model):
-            pass
-        self.assertIs(self.apps.get_model('app_label', 'TestModel'), TestModel)
-
-
+#     def test_model_definition(self):
+#         """."""
+#         class TestModel(models.Model):
+#             pass
+#         self.assertIs(self.apps.get_model(
+#             'app_label', 'TestModel'), TestModel)
--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -2,32 +2,20 @@
 
 from django.test import TestCase, Client, RequestFactory
 from django.contrib.auth.models import User
-
-
 from imager_profile.models import ImagerProfile
-from imager_images.tests import UserFactory, ImageFactory, AlbumFactory
-from django.urls import reverse_lazy
-import factory
-from faker import Faker
 
-fake = Faker()
+import factory
+# from random import random
+# from faker import Faker
+
+# fake = Faker()
+
 
 # Create your tests here.
 
 
 class ProfileTestCase(TestCase):
     """Profile model test runner."""
-
-    def fake_profile_attrs(profile):
-        """."""
-        profile.address = fake.address()
-        profile.bio = fake.paragraph()
-        profile.website = fake.url()
-        profile.travel_radius = 20 * random.random()
-        profile.home = random.randint( 11111111, 00000000000)
-        profile.photography_type = "LS"
-        profile.save()
-
 
     class UserFactory(factory.django.DjangoModelFactory):
         """Factory for building new user objects."""
@@ -39,16 +27,14 @@ class ProfileTestCase(TestCase):
 
         username = factory.Sequence(lambda n: "Bob {}".format(n))
         email = factory.LazyAttribute(
-            lambda x: "{}@foo.com".format(x.username.replace(" ", ""))
+            lambda x: "{}@imager.com".format(x.username.replace(" ", ""))
         )
 
     def setUp(self):
         """Setup for the test."""
         self.users = [self.UserFactory.create() for i in range(20)]
-        for profile in ImagerProfile.objects.all():
-                fake_profile_attrs(profile)
-
-
+        # for profile in ImagerProfile.objects.all():
+        #     fake_profile_attrs(profile)  # <--------------- what's this?
 
     def test_profile_created(self):
         """Test that ImagerProfile object is created once user is saved."""
@@ -86,131 +72,32 @@ class ProfileTestCase(TestCase):
         this_user = self.users[0]
         this_user.is_active = False
         this_user.save()
-        self.assertTrue(Imagerprofile.active.count() == User.objects.count() - 1)
+        self.assertTrue(
+            ImagerProfile.active.count() == User.objects.count() - 1)
 
-    def test_update_profile(self):
-        """Test that a profile update also updates db."""
-        # self.users[0].profile.bio = "This is mo betta."
-        # query = User.objects.first()
-        # self.assertTrue(
-        #     query.profile.bio == "This is mo betta.")
-        pass
+    # def test_update_profile(self):
+    #     """Test that a profile update also updates db."""
+    #     self.users[0].profile.bio = "This is mo betta."
+    #     query = User.objects.first()
+    #     self.assertTrue(
+    #         query.profile.bio == "This is mo betta.")
+    #     pass
 
-    def test_changes_on_profile_mean_changes_on_user_profile(self):
-        """."""
-        this_user = User.objects.first()
-        this_profile = Imagerprofile.objects.get(user=this_user)
-        this_profile.photography_type = "PT"
-        this_profile.save()
-        # import pdb; pdb.set_tract()
-        self.assertTrue(this_user.profile.photography_type == "PT")
+    # def test_changes_on_profile_mean_changes_on_user_profile(self):
+    #     """."""
+    #     this_user = User.objects.first()
+    #     this_profile = ImagerProfile.objects.get(user=this_user)
+    #     this_profile.photography_type = "PT"
+    #     this_profile.save()
+    #     self.assertTrue(this_user.profile.photography_type == "PT")
 
-    def test_del_user_on_db_and_profile(self):
-        """Test delete user on DB & Imgr.
+    # def test_del_user_on_db_and_profile(self):
+    #     """Test delete user on DB & Imgr.
 
-        Ensure that if a user is deleted from the database, the ImagerProfile
-        associated with that user is also deleted.
-        """
-        pass
-
-class ProfileFrontEndTests(TestCase):
-    """."""
-
-    def setUp(self):
-        """."""
-        self.client = Client()
-        self.request = RequestFactory()
-
-        def test_how_view_is_status_ok(self):
-            """."""
-            from imagersite.views import home_view
-            # from imager_profile.views import home_view
-            req = self.request.get("/potato")
-            response = home_view(req)
-            self.assertTrue(response.status_code == 200)
-
-        def test_home_route_is_status_ok(self):
-            """."""
-            response = self.client.get("/")
-            self.assertTrue(response.status_code == 200)
-
-        def test_home_route_context_foo(self):
-            """."""
-            respponse = self.client.get("/")
-            self.assertTrue(response.conext["foo"] == "bar")
-
-        def test_home_route_uses_right_templates(self):
-            """."""
-            response = self.client.get("/")
-            self.assertTemplateUsed(response, "imagersite/home.html")
-            self.assertTemplateUsed(response, "Imagersite/base.html")
-
-        def test_login_route_redirects(self):
-            """."""
-            new_user = UserFactory.create()
-            new_user.username = "potato_joe"
-            new_user.set_password("tugboats")
-            new_user.save()
-            # import pdb; pdb.set_trace()
-            response = self.client.post("/login/", {
-                "username": new_user.username,
-                "password": "tugboats"
-            })
-            self.assertTrue(response.status_code == 302)
-
-         def test_login_route_redirects_to_homepage(self):
-            """."""
-            new_user = UserFactory.create()
-            new_user.username = "potato_joe"
-            new_user.set_password("tugboats")
-            new_user.save()
-            # import pdb; pdb.set_trace()
-            response = self.client.post("/login/", {
-                "username": new_user.username,
-                "password": "tugboats"
-            }, follow=True)
-            self.assertTrue(response.redirect_chain[0][0] == "/"
-                status_code == 302)
-
-        def register_new_user(self):
-            """Test registering new user works."""
-            self.client.post("/registration/register/", {
-                "username": "bobdobson",
-                "email": "bob@dob.son",
-                "pasword1": "tugboats",
-                "password2": "tugboats",
-            }, follow=follow)
-            return response
-
-        def test_can_register_new_user(self):
-            """."""
-            self.assertTrue(User.objets.count() == 0)
-            self.register_new_user()
-            self.assertTrue(User.objects.count() == 1)
-
-        def test_registered_user_is_inactive(self):
-            """."""
-            self.register_new_user()
-            the_user = User.objects.first()
-            self.assertFalse(the_user.is_active)
-
-         def test_successful_registeration_redirect(self):
-            """."""
-            self.register_new_user()
-            self.assertTrue(response.status_code == 302)
-
-        def test_successful_registration_redirecs_to_right_place(self):
-            """."""
-            response = self.register_new_user(follow=True)
-            self.assertTrue(response.redirec_chain[0][0] == "/registration/register/complete/") # <---- check
-
-
-
-    # def test_profile_is_active(self):
-    #     """Test profile.is_active is active."""
-    #     for i in range(20):
-    #         user = self.users[i]
-    #         # self.assertTrue(user.is_active)
+    #     Ensure that if a user is deleted from the database, the ImagerProfile
+    #     associated with that user is also deleted.
+    #     """
+    #     pass
 
     def test_profile_is_active(self):
         """Test profile.is_active is active."""
@@ -223,3 +110,126 @@ class ProfileFrontEndTests(TestCase):
         for i in range(20):
             user = str(self.users[i])
             self.assertTrue('Bob', '@imager.com' in user)
+
+
+# =================== FRONT END TESTS ======================================= #
+
+
+class ProfileFrontEndTests(TestCase):
+    """Testing for front end tests."""
+
+    class UserFactory(factory.django.DjangoModelFactory):
+        """Factory for building new user objects."""
+
+        class Meta:
+            """Setup factory model."""
+
+            model = User
+
+        username = factory.Sequence(lambda n: "Bob {}".format(n))
+        email = factory.LazyAttribute(
+            lambda x: "{}@foo.com".format(x.username.replace(" ", ""))
+        )
+
+    def setUp(self):
+        """."""
+        self.client = Client()
+        self.request = RequestFactory()
+
+    def test_how_view_is_status_ok(self):
+        """."""
+        from imagersite.views import home_view
+        # from imager_profile.views import home_view
+        req = self.request.get("/potato")
+        response = home_view(req)
+        self.assertTrue(response.status_code == 200)
+
+    def test_home_route_is_status_ok(self):
+        """."""
+        response = self.client.get("/")
+        self.assertTrue(response.status_code == 200)
+
+    # def test_home_route_context_foo(self):
+    #     """."""
+    #     response = self.client.get("/")
+    #     self.assertTrue(response.context["foo"] == "bar")
+
+    def test_home_route_uses_right_templates(self):
+        """."""
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "imagersite/home.html")
+
+    def test_login_route_redirects(self):
+        """Test login route redirects."""
+        new_user = self.UserFactory.create()
+        new_user.username = "potato_joe"
+        new_user.set_password("tugboats")
+        new_user.save()
+        # import pdb; pdb.set_trace()
+        response = self.client.post("/login/", {
+            "username": new_user.username,
+            "password": "tugboats"
+        })
+        self.assertTrue(response.status_code == 302)
+
+    def test_login_route_redirects_to_homepage(self):
+        """Test login route redirects to homepage."""
+        new_user = self.UserFactory.create()
+        new_user.username = "potato_joe"
+        new_user.set_password("tugboats")
+        new_user.save()
+        # import pdb; pdb.set_trace()
+        response = self.client.post("/login/", {
+            "username": new_user.username,
+            "password": "tugboats"
+        }, follow=True)
+        self.assertRedirects(
+            response, "/", status_code=302, target_status_code=200
+        )
+
+    # def register_new_user(self):
+    #     """Test registering new user works."""
+    #     self.client.post("/registration/register/", {
+    #         "username": "bobdobson",
+    #         "email": "bob@dob.son",
+    #         "pasword1": "tugboats",
+    #         "password2": "tugboats",
+    #     }, follow=follow)  # <--------- follow and...
+    #     return response  # <--------- response are undefined?
+    #
+    #     the following tests don't work because the above method is broken
+
+    # def test_can_register_new_user(self):
+    #     """."""
+    #     self.assertTrue(User.objects.count() == 0)
+    #     self.register_new_user()
+    #     self.assertTrue(User.objects.count() == 1)
+
+    # def test_registered_user_is_inactive(self):
+    #     """."""
+    #     self.register_new_user()
+    #     the_user = User.objects.first()
+    #     self.assertFalse(the_user.is_active)
+
+    #  def test_successful_registeration_redirect(self):
+    #     """."""
+    #     self.register_new_user()
+    #     self.assertTrue(response.status_code == 302)
+
+    # def test_successful_registration_redirecs_to_right_place(self):
+    #     """Test successful registration redirects to right place."""
+    #     response = self.register_new_user(follow=True)
+    #     self.assertTrue(
+    #     response.redirec_chain[0][0] == "/registration/register/complete/")
+
+    # def test_profile_is_active(self):
+    #     """Test profile.is_active is active."""
+    #     for i in range(20):
+    #         user = self.users[i]
+    #         self.assertTrue(user.is_active)
+
+    # def test_string_returns_profile_info(self):
+    #     """Test if the string method returns matching profile info."""
+    #     for i in range(20):
+    #         user = str(self.users[i])
+    #         self.assertTrue('Bob', '@imager.com' in user)
