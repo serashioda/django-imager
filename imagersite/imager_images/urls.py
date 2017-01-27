ca28900feaf9ade0efed08446cb2b050e6c3704d
Your Name <you@example.com>
2017-01-27 08:48:09 -0800

    urls.py added and routes created. templates folder created with appropriate html files. views underway.

---
+++ imagersite/imager_images/urls.py
@@ -0,0 +1,11 @@
+"""Images url paths."""
+from django.conf.urls import url
+from .views import single_photo, all_photos, single_album, all_albums, library
+
+urlpatterns = [
+    url(r'^photos/(?P<photo_id>\d+)', single_photo, name='show_photo'),
+    url(r'^photos/$', all_photos, name='list_photos'),
+    url(r'^albums/(?P<album_id>|d+)', single_album, name='show_album'),
+    url(r'^albums/?', all_albums, name='list_albums'),
+    url(r'^library$', library, name='library'),
+]
--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -1,3 +1,30 @@
+"""Views for images."""
 from django.shortcuts import render
+from imager_images.models import Photo, Album
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def single_photo(request, photo_id):
+    """."""
+    photo = Photo.objects.get(id=photo_id)
+    if photo.published == 'private' and photo.user_id == request.user.id:
+        return HttpResponse('Unauthorized', status=401)
+    return render(request, 'imager_images/photo.html', {"photo": photo})
+
+
+def all_photos(request):
+    """."""
+
+
+def single_album(request, album_id):
+    """."""
+
+
+def all_albums(request):
+    """."""
+
+
+@login_required(login_url='/accounts/login/')
+def library(request):
+    """Library view."""
