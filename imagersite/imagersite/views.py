f43839b716b1feadfc0f5ee2d9cb45ee9e062bbc
Ben Petty <benjamin.s.petty@gmail.com>
2017-02-01 16:23:20 -0800

    more front end edits

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -8,4 +8,3 @@ from django.shortcuts import render
 def home_view(request):
     """Home view."""
     return render(request, 'imagersite/home.html')
-    # return render(request, 'imagersite/home.html', {'foo': bar})
