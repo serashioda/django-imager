73fd0f984f88237686a32e3eb641f01a66e50a7d
Ben Petty <benjamin.s.petty@gmail.com>
2017-02-01 22:54:10 -0800

    fix random error on home view unit tests

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -15,6 +15,9 @@ def home_view(request):
         if (photo.published != 'private' or
                 photo.user.username == request.user.username):
             public_photos.append(photo)
-    idx = random.randint(0, (len(public_photos) - 1))
-    photo = public_photos[idx]
+    if len(public_photos) > 0:
+        idx = random.randint(0, (len(public_photos) - 1))
+        photo = public_photos[idx]
+    else:
+        photo = None
     return render(request, 'imagersite/home.html', {"photo": photo})
