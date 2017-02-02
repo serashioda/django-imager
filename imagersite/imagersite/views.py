f3f0859e95226dfec116f4358be54040f02ed9ca
Ben Petty <benjamin.s.petty@gmail.com>
2017-02-01 18:05:02 -0800

    incl random images on home screen

--- imagersite/imagersite/views.py
+++ imagersite/imagersite/views.py
@@ -1,10 +1,20 @@
 """Views."""
 
 from django.shortcuts import render
+from imager_images.models import Photo
+import random
 
 # Create your views here.
 
 
 def home_view(request):
     """Home view."""
-    return render(request, 'imagersite/home.html')
+    public_photos = []
+    photos = Photo.objects.all()
+    for photo in photos:
+        if (photo.published != 'private' or
+                photo.user.username == request.user.username):
+            public_photos.append(photo)
+    idx = random.randint(0, (len(public_photos) - 1))
+    photo = public_photos[idx]
+    return render(request, 'imagersite/home.html', {"photo": photo})
