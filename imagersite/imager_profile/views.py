25ac3bb1cd86c7a0e7d7ed429723a0e0755f673a
Your Name <you@example.com>
2017-02-11 22:03:04 -0800

    added classed based views for imager_profile, updated profile template with private and public mode.

--- imagersite/imager_profile/views.py
+++ imagersite/imager_profile/views.py
@@ -1,20 +1,39 @@
 """Imager profile view."""
-from django.shortcuts import render
-from imager_profile.models import ImagerProfile
+# from imager_profile.models import ImagerProfile
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView
 
 
-def profile_view(request):
+class Profile(LoginRequiredMixin, ListView):
     """Profile view."""
+
     print('just mine')
-    # profile = ImagerProfile.active.filter(user__username=username).first()
-    photos = request.user.photos.all().order_by('-id')
-    return render(request, 'imager_profile/profile.html', {'photos': photos})
+    login_url = reverse_lazy('login')
+
+    model = User
+    template_name = "imager_profile/profile.html"
+
+    # # profile = ImagerProfile.active.filter(user__username=username).first()
+    # photos = request.user.photos.all().order_by('-id')
+    # # import pdb; pdb.set_trace()
+    # return render(request, 'imager_profile/profile.html', {'photos': photos})
 
 
-def public_profile_view(request, username):
+class PublicProfile(ListView):
     """Public profile view."""
+
     print('public yo')
-    print('username:', username)
-    profile = ImagerProfile.active.filter(user__username=username).first()
-    photos = request.user.photos.all().order_by('-id')
-    return render(request, 'imager_profile/public_profile.html', {'profile': profile}, {'photos': photos})
+    # print('username:', username)
+
+    model = User
+    template_name = "imager_profile/profile.html"
+
+    def get_context_data(self):
+        """Return user object."""
+        return {"user": User.objects.get(username=self.kwargs['username'])}
+
+    # profile = ImagerProfile.active.filter(user__username=username).first()
+    # photos = request.user.photos.all().order_by('-id')
+    # return render(request, 'imager_profile/public_profile.html', {'profile': profile}, {'photos': photos})
